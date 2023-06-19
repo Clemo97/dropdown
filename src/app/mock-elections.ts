8d9ab5f8eaccf7a97275584c7c53a864e0229596
Clemo97 <lumumbaclement@gmail.com>
2023-06-19 16:13:17 +0300

    add more election data

--- src/app/mock-elections.ts
+++ src/app/mock-elections.ts
@@ -55,6 +55,195 @@ export const ELECTIONS: County[] = [
       { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
     ],
   },
+  {
+    name: 'Nairobi',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
+  {
+    name: 'Kisumu',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
+  {
+    name: 'Kisii',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
+  {
+    name: 'Kakamega',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
+  {
+    name: 'Kajiado',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
+  {
+    name: 'Nandi',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
+  {
+    name: 'Wajir',
+    president: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 13400 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 54000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 16450 },
+      { candidate: 'Candidate 4', party: 'Party D', votes: 2340 },
+    ],
+    governor: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 10000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 3', party: 'Party C', votes: 6000 },
+      { candidate: 'Candidate 4', party: 'INDEPENDENT', votes: 4000 },
+    ],
+    senate: [
+      { candidate: 'Candidate 1', party: 'UDA', votes: 9000 },
+      { candidate: 'Candidate 2', party: 'ODM', votes: 7000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 5000 },
+      { candidate: 'Candidate 4', party: 'JUBILEE', votes: 3000 },
+    ],
+    womenRepresentative: [
+      { candidate: 'Candidate 1', party: 'ODM', votes: 8000 },
+      { candidate: 'Candidate 2', party: 'UDA', votes: 5000 },
+      { candidate: 'Candidate 3', party: 'INDEPENDENT', votes: 4000 },
+      { candidate: 'Candidate 4', party: 'Party L', votes: 2000 },
+    ],
+  },
 ];
 export { County };
 
